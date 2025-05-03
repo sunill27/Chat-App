4aea4a2816c2a11d46e33d2a873314ec7be98f11
sunill27 <sunilchand675@gmail.com>
2025-05-03 16:00:07 +0545

    signUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import { authStore } from "../store/authStore";
-import { MessageSquare, User } from "lucide-react";
+import {
+  Eye,
+  EyeOff,
+  Loader2,
+  Lock,
+  Mail,
+  MessageSquare,
+  User,
+} from "lucide-react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState();
@@ -48,7 +57,7 @@ const SignUp = () => {
                 </div>
                 <input
                   type="text"
-                  className="{`imput input-bordered w-full pl-10"
+                  className={`input input-bordered w-full pl-10`}
                   placeholder="John Doe"
                   value={formData.fullName}
                   onChange={(e) =>
@@ -57,9 +66,89 @@ const SignUp = () => {
                 />
               </div>
             </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 items-center pointer-events-none">
+                  <Mail className="size-5 text-base-control/40" />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="example@gmail.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 items-center pointer-events-none">
+                  <Lock className="size-5 text-base-control/40" />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="********"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={isSigningUp}
+            >
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
           </form>
+
+          <div className="text-center">
+            <p className="text-base-content/60">
+              Already have an account?
+              <Link to="/login" className="link link-primary">
+                Sign In
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
+
+      {/* Right Side */}
     </div>
   );
 };
