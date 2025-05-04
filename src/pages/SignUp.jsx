8923bd029db66aa30329e70da43c6fdba742f664
sunill27 <sunilchand675@gmail.com>
2025-05-04 10:26:04 +0545

    nav page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -53,7 +53,7 @@ const SignUp = () => {
     }
   };
   return (
-    <div className="min-h-screen grid lg:grid-cols-2">
+    <div className="min-h-screen grid lg:grid-cols-2 mt-12">
       {/* Left side: */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
--- src/store/authStore.js
+++ src/store/authStore.js
@@ -36,4 +36,29 @@ export const authStore = create((set) => ({
       set({ isSigningUp: false });
     }
   },
+
+  //Login Function:
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  //Logout Function:
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 }));
